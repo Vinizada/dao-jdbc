8aeb5dc86233d742dc363df0624d9bad30c06fb4
Vinicius Lima <vinislimadasilva17@gmail.com>
2021-07-02 14:42:21 -0300

    Insert Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import mode.entities.Department;
@@ -37,6 +38,13 @@ public class Program {
 		for (Seller obj : listTeste) {
 			System.out.println(obj);
 		}
+		
+		System.out.println();
+		System.out.println("=== TEST 4: seller insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted: Id = " + newSeller.getId());
+		
 
 	}
 
